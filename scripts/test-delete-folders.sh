9eac9559a1405692b8f64cf7d965ce6569e498fd
Bock Yang <bockyang.govtech@gmail.com>
2020-07-26 17:03:20 +0800

    fixing test issues

--- scripts/test-delete-folders.sh
+++ scripts/test-delete-folders.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
+function check_configuration {
+  if [[ -z "${PROJECT_RELATIVE_FOLDER}" ]]; then
+    echo "Could not find required configuration [PROJECT_RELATIVE_FOLDER]. Exiting"
+    exit 1
+  fi
+}
 
-echo "Deleting test-folder..."
-rm -rf test-folder
-echo "Deleted!"
+function delete_folder() {
+  echo "Deleting test-folder..."
+  rm -rf "${PROJECT_RELATIVE_FOLDER}"/test-folder
+  echo "Deleted!"
+  exit 0
+}
+
+check_configuration
+delete_folder
+
+
--- scripts/test-prep-movie.sh
+++ scripts/test-prep-movie.sh
@@ -1,40 +1,47 @@
 #!/usr/bin/env bash
 
+function check_configuration {
+  if [[ -z "${PROJECT_RELATIVE_FOLDER}" ]]; then
+    echo "Could not find required configuration [PROJECT_RELATIVE_FOLDER]. Exiting"
+    exit 1
+  fi
+}
+
 function make_base_folders {
   echo "Creating base folder: test-folder..."
-  mkdir -p test-folder/downloads
-  mkdir -p test-folder/movies
-  mkdir -p test-folder/tv
-  mkdir -p test-folder/others
-  mkdir -p test-folder/apptemp
+  mkdir -p "${PROJECT_RELATIVE_FOLDER}"/test-folder/downloads
+  mkdir -p "${PROJECT_RELATIVE_FOLDER}"/test-folder/movies
+  mkdir -p "${PROJECT_RELATIVE_FOLDER}"/test-folder/tv
+  mkdir -p "${PROJECT_RELATIVE_FOLDER}"/test-folder/others
+  mkdir -p "${PROJECT_RELATIVE_FOLDER}"/test-folder/apptemp
   echo "Created!"
 }
 
 function delete_base_folders {
   echo "Deleting test-folder..."
-  rm -rf test-folder
+  rm -rf "${PROJECT_RELATIVE_FOLDER}"/test-folder
   echo "Deleted!"
 }
 
-
+check_configuration
 delete_base_folders
 make_base_folders
 
 # Movie Sample 1
 baseFolderName="Avengers.Endgame.2019.2160p.BluRay.x265.10bit.SDR.DTS-HD.MA.TrueHD.7.1.Atmos-SWTYBLZ"
-mkdir -p test-folder/downloads/${baseFolderName}
-mkdir -p test-folder/downloads/${baseFolderName}/Sample
-touch test-folder/downloads/${baseFolderName}/Sample/Sample-AEBSM10.mkv
-touch test-folder/downloads/${baseFolderName}/${baseFolderName}.mkv
-touch test-folder/downloads/${baseFolderName}/RARBG.txt
+mkdir -p "${PROJECT_RELATIVE_FOLDER}"/test-folder/downloads/${baseFolderName}
+mkdir -p "${PROJECT_RELATIVE_FOLDER}"/test-folder/downloads/${baseFolderName}/Sample
+touch "${PROJECT_RELATIVE_FOLDER}"/test-folder/downloads/${baseFolderName}/Sample/Sample-AEBSM10.mkv
+touch "${PROJECT_RELATIVE_FOLDER}"/test-folder/downloads/${baseFolderName}/${baseFolderName}.mkv
+touch "${PROJECT_RELATIVE_FOLDER}"/test-folder/downloads/${baseFolderName}/RARBG.txt
 
 # Movie Sample 2
 baseFolderName="Sonic.the.Hedgehog.2020.2160p.UHD.BluRay.x265.10bit.HDR.DTS-HD.MA.TrueHD.7.1.Atmos-SWTYBLZ"
-mkdir -p test-folder/downloads/${baseFolderName}
-mkdir -p test-folder/downloads/${baseFolderName}/Sample
-touch test-folder/downloads/${baseFolderName}/Sample/Sample-STHBHM10.mkv
-touch test-folder/downloads/${baseFolderName}/${baseFolderName}.mkv
-touch test-folder/downloads/${baseFolderName}/RARBG.txt
+mkdir -p "${PROJECT_RELATIVE_FOLDER}"/test-folder/downloads/${baseFolderName}
+mkdir -p "${PROJECT_RELATIVE_FOLDER}"/test-folder/downloads/${baseFolderName}/Sample
+touch "${PROJECT_RELATIVE_FOLDER}"/test-folder/downloads/${baseFolderName}/Sample/Sample-STHBHM10.mkv
+touch "${PROJECT_RELATIVE_FOLDER}"/test-folder/downloads/${baseFolderName}/${baseFolderName}.mkv
+touch "${PROJECT_RELATIVE_FOLDER}"/test-folder/downloads/${baseFolderName}/RARBG.txt
 
 # Movie Sample 3 : No folder
-touch "test-folder/downloads/Spirited.Away.2001.RERIP.1080p.BluRay.X264-AMIABLE[rarbg].mkv"
+touch "${PROJECT_RELATIVE_FOLDER}"/"test-folder/downloads/Spirited.Away.2001.RERIP.1080p.BluRay.X264-AMIABLE[rarbg].mkv"
